e2370d4b2c1da6ce9c32b2b3015d90a92cf87650
s.orhanselcuk@gmail.com <s.orhanselcuk@gmail.com>
2023-08-24 12:21:59 +0300

    DateTimeExtension.FromJulianDate(this string value) fonksiyonunda oluşan outOfRange hatası giderilmiştir

--- Concrete/DateTimeExtension.cs
+++ Concrete/DateTimeExtension.cs
@@ -9,8 +9,7 @@ namespace ConvertExtensionLibrary.Concrete
         {
             int year = string.Join("",value.Take(4)).AsInteger();
             int day = string.Join("", value.Skip(4)).AsInteger();
-            DateTime resultDateTime = new DateTime(year,0,0);
-            resultDateTime.AddDays(day);
+            DateTime resultDateTime = new DateTime(year,1,1).AddDays((day - 1));
             return resultDateTime;
         }
     }
